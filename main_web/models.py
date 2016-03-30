2c0c8d6100b637289089c3c7533bf0b49a8283b3
waterwindwood <469798015@qq.com>
2016-03-30 23:11:10 +0800

    [+]允许后台编辑模版

--- main_web/models.py
+++ main_web/models.py
@@ -11,3 +11,5 @@ class Stencil(models.Model):
     CREATOR = models.CharField(max_length = 100)
     echarts_737_7 = models.CharField(max_length = 1000)
     echarts_737_3C = models.CharField(max_length = 1000)
+    def __unicode__(self):              # __str__ on Python 3
+        return self.NAME
