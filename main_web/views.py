2f7b36b4188e710f2bb6aaae801ea6b9aff8af3a
waterwindwood <469798015@qq.com>
2016-06-04 00:46:21 +0800

    [+]增加首页最新航段数据

--- main_web/views.py
+++ main_web/views.py
@@ -3,6 +3,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from influxdb_function import influxDB_interface
+
 
 def home(request):
-    return render(request, 'home.html')
+    infdb_if = influxDB_interface()
+    sector_index = infdb_if.limit_query("DB_sector_index", "*", "index")
+    df = sector_index['index']
+    result_json = df.to_json(orient="records")
+    return render(request, 'home.html',{'result_json': result_json})
