a3b7191f61210169964985f1788cf7301fe0d859
waterwoodwind <469798015@qq.com>
2016-06-16 09:25:22 +0800

    [+]首页最新航段数据改为当日航段数据提升速度

--- main_web/views.py
+++ main_web/views.py
@@ -2,13 +2,23 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from arrow_time import today_date_for_influxd_sql
+from arrow_time import ten_day_ago_for_influxd_sql
 from influxdb_function import influxDB_interface
 
 
 def home(request):
+    date_start = today_date_for_influxd_sql()
+    date_end = today_date_for_influxd_sql()
+
+
+    where_str = " WHERE time > " + "'" + date_start + "'" + " - 8h" + " AND time < " + "'" + date_end + "'" + " + 16h"
     infdb_if = influxDB_interface()
-    sector_index = infdb_if.limit_query("DB_sector_index", "*", "index")
-    df = sector_index['index']
-    result_json = df.to_json(orient="records")
-    return render(request, 'home.html',{'result_json': result_json})
+    sector_index = infdb_if.inf_query("DB_sector_index", "*", "index", where_str)
+    if sector_index <> {}:
+        df = sector_index['index']
+        result_json = df.to_json(orient="records")
+        return render(request, 'home.html', {'result_json': result_json})
+    else:
+
+        return render(request, 'home.html', {'result_json': {}})
